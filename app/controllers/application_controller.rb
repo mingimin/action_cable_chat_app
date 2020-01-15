0bbe77ae922360f0554e93ff3c8134827e6faee8
lapis <omoikane09@gmail.com>
2020-01-15 22:47:42 +0900

    5. Basic enhancements 5.1 Login Protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
     # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
+  end
 end
