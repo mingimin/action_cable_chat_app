88a20d49064e45d9d0e6766840f47fd2f16c02ff
lapis <omoikane09@gmail.com>
2020-01-21 08:54:58 +0900

    5.3 message_rendering Finish basic enhancements

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,8 +10,7 @@ class MessagesController < ApplicationController
     return unless message.save
 
     ActionCable.server.broadcast 'room_channel',
-                                 content: message.content,
-                                 username: message.user.username
+                                 message: render_message(message)
     head :ok
   end
 
@@ -24,5 +23,9 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:content)
+  end
+
+  def render_message(message)
+    render(partial: 'message', locals: { message: message })
   end
 end
