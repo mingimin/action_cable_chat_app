50695fb19e43d5e16808c55199accdfed4cd6118
lapis <omoikane09@gmail.com>
2020-01-23 11:59:00 +0900

    6.2 at mention notifications

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,10 @@ class Message < ApplicationRecord
   belongs_to :user
   validates :content, presence: true
   scope :for_display, -> { order(:created_at).last(50) }
+
+  # Returns a list of users @mentioned in message content.
+  def mentions
+    names = content.scan(/@(#{User::NAME_REGEX})/).flatten
+    User.where(username: names)
+  end
 end
